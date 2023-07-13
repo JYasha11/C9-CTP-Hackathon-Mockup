535b1ed84836ce603318650b5fe22cf25729db8e
Jon <jonathanyashayev11@gmail.com>
2023-07-13 15:37:56 -0400

    navbar/banner additions

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import Navbar from "./components/navbar"
+import Banner from "./components/banner"
+
 
 function App() {
   return (
     <div>
-      
+      <Banner />
+      <Navbar />
     </div>
   );
 }
